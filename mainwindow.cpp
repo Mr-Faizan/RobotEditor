2d650dc9c01e5924ff07c1d05906c71e9595e60d
Ahmed <fahmed@iwu.fraunhofer.de>
2025-01-20 18:06:35 +0100

    #issue 6 Commit 8 . Add the 3D window for Robots and also add Template Json file and QTreeview code

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,14 +1,67 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
 
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    // First we need to add the template of Robot data from the file.
+    addRobotDataTemplate();
+
+
+    // Setup the 3D Playground
+    setup3DPlayground();
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+// Add the Robot Data Template
+void MainWindow::addRobotDataTemplate() {
+
+    QFile file(":/Resources/Json/Template.json");
+
+    if (!file.open(QIODevice::ReadOnly) | QIODevice::Text) {
+        qWarning("Template file is missing. Please add the template file.");
+        return;
+    }
+
+    QByteArray templateJson = file.readAll();
+    file.close();
+
+
+    QJsonDocument templateDocument = QJsonDocument::fromJson(templateJson);
+    QJsonObject templateObject = templateDocument.object();
+
+    
+    QStandardItemModel *model = new QStandardItemModel(0, 2, this);
+}
+
+// Setup the Main Playground for 3D Viewer
+void MainWindow::setup3DPlayground() {
+
+    // First creating 3D window
+    view = new Qt3DExtras::Qt3DWindow();
+    rootEntity = new Qt3DCore::QEntity();
+
+    // Set the background color
+    view->defaultFrameGraph()->setClearColor(QColor(Qt::darkCyan));
+
+    // Set the root Entity
+    view->setRootEntity(rootEntity);
+
+    // Create a window container for view
+    QWidget *container = QWidget::createWindowContainer(view);
+
+    // Now its time to show this view on frontend.
+    QVBoxLayout *layout = new QVBoxLayout();
+    layout->addWidget(container);
+    ui->viewContainer->setLayout(layout);
+}
+
+
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,27 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QWidget>
+
+// Qt Libraries
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QJsonArray>
+#include <QFile>
+#include <QTreeView>
+#include <QStandardItemModel>
+
+
+// 3D Libraries
+#include <Qt3DCore/QEntity>
+#include <Qt3DExtras/Qt3DWindow>
+#include <Qt3DExtras/qforwardrenderer.h>
+#include <QWidget>
+#include <QVBoxLayout>
+#include <QColor>
+
+using namespace std;
+
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -19,5 +40,13 @@ public:
 
 private:
     Ui::MainWindow *ui;
+    Qt3DExtras::Qt3DWindow *view;
+    Qt3DCore::QEntity *rootEntity;
+
+    void addRobotDataTemplate();
+
+    // 3D Window Playground
+    void setup3DPlayground();
+    
 };
 #endif // MAINWINDOW_H
